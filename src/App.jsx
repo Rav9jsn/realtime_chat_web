5d92e89f36b15b503da1a2ec1751608fc2afaaef
Rav9jsn <kravi402253@gmail.com>
2025-04-28 17:35:47 +0530

    ok#

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold bg-red-400 underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBD26o5_RdjhrmFTWMRDhXSBfJz5FOXYoI",
+  authDomain: "whatsupclone-7d43d.firebaseapp.com",
+  projectId: "whatsupclone-7d43d",
+  storageBucket: "whatsupclone-7d43d.firebasestorage.app",
+  messagingSenderId: "766773227282",
+  appId: "1:766773227282:web:7414c0a73612db33b34dcb",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
